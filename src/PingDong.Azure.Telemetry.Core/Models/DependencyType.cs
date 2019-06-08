814bffc2a37facd16ed984262886e5922d70754b
Ping Dong <ping.dong@live.com>
2019-06-08 15:56:17 +1200

    Redesign Telemetry service

---
+++ src/PingDong.Azure.Telemetry.Core/Models/DependencyType.cs
@@ -0,0 +1,9 @@
+﻿namespace PingDong.Azure.Telemetry
+{
+    public struct DependencyType
+    {
+        public const string WebAPI = "WebAPI";
+        public const string SQLQuery = "SQLQuery";
+        public const string Method = "Method";
+    }
+}
--- src/PingDong.Azure.Telemetry.Core/Service/PassThroughTelemetryService.cs
+++ src/PingDong.Azure.Telemetry.Core/Service/PassThroughTelemetryService.cs
@@ -7,31 +7,69 @@ namespace PingDong.Azure.Telemetry
     public class PassThroughTelemetryService : ITelemetryService
     {
         /// <inheritdoc />
-        public Task<HttpResponseMessage> TrackApiCallAsync(HttpRequestMessage request, Func<Task<HttpResponseMessage>> func)
+        public Task<HttpResponseMessage> TrackWebApiAsync(string correlationId, HttpRequestMessage request, Func<Task<HttpResponseMessage>> func)
         {
             return func();
         }
         
         /// <inheritdoc />
-        public Task<HttpResponseMessage> TrackApiCallAsync(string requestMethod, Uri requestUri, Func<Task<HttpResponseMessage>> func)
+        public Task<HttpResponseMessage> TrackWebApiAsync(string correlationId, string requestMethod, Uri requestUri, Func<Task<HttpResponseMessage>> func)
         {
             return func();
         }
         
         /// <inheritdoc />
-        public Task<HttpResponseMessage> TrackApiCallAsync(string requestMethod, string requestUri, Func<Task<HttpResponseMessage>> func)
+        public Task<HttpResponseMessage> TrackWebApiAsync(string correlationId, string requestMethod, string requestUri, Func<Task<HttpResponseMessage>> func)
         {
             return func();
         }
-
-        public Task<T> TrackCallAsync<T>(string target, string funcName, string data, Func<Task<T>> func)
+        
+        /// <inheritdoc />
+        public Task<T> TrackFunctionAsync<T>(string function, string type, string target, string data, Func<Task<T>> func)
         {
             return func();
         }
-
-        public Task TrackCallAsync(string target, string funcName, string data, Func<Task> func)
+        
+        /// <inheritdoc />
+        public Task<T> TrackFunctionAsync<T>(string function, string type, string target, Func<Task<T>> func)
         {
             return func();
+        }
+        
+        /// <inheritdoc />
+        public Task<T> TrackFunctionAsync<T>(string function, string type, Func<Task<T>> func)
+        {
+            return func();
+        }
+        
+        /// <inheritdoc />
+        public Task<T> TrackFunctionAsync<T>(string correlationId, string function, string type, string target, string data, Func<Task<T>> func)
+        {
+            return func();
+        }
+        
+        /// <inheritdoc />
+        public Task TrackActionAsync(string function, string type, string target, string data, Func<Task> action)
+        {
+            return action();
+        }
+        
+        /// <inheritdoc />
+        public Task TrackActionAsync(string function, string type, string target, Func<Task> action)
+        {
+            return action();
+        }
+        
+        /// <inheritdoc />
+        public Task TrackActionAsync(string function, string type, Func<Task> action)
+        {
+            return action();
+        }
+        
+        /// <inheritdoc />
+        public Task TrackActionAsync(string correlationId, string function, string type, string target, string data, Func<Task> action)
+        {
+            return action();
         }
     }
 }
